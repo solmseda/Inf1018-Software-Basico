82c17c21d69a57c82af20c2700ef8f6b972d4185
Sol <solmseda@gmail.com>
2023-10-19 10:11:06 -0300

    função de teste dando segmentation fault

--- trabalho1/main.c
+++ trabalho1/main.c
@@ -5,16 +5,16 @@
 int main() {
     FILE *arq_entrada_utf8 = fopen("utf8_peq.txt", "rb");
     FILE *arq_saida_varint = fopen("utf2varint_saida.txt", "wb");
-    FILE *arq_entrada_varint = fopen("utf2varint_saida", "rb");
-    FILE *arq_saida_utf8 = fopen("varint2utf_saida.txt", "wb");
 
     utf2varint(arq_entrada_utf8, arq_saida_varint);
 
     fclose(arq_entrada_utf8);
     fclose(arq_saida_varint);
 
-    varint2utf(arq_entrada_varint, arq_saida_utf8);
+    FILE *arq_entrada_varint = fopen("var_peq", "rb");
+    FILE *arq_saida_utf8 = fopen("varint2utf_saida.txt", "wb");
 
+    varint2utf(arq_entrada_varint, arq_saida_utf8);
 
     fclose(arq_entrada_varint);
     fclose(arq_saida_utf8);
--- trabalho1/teste.c
+++ trabalho1/teste.c
@@ -4,39 +4,65 @@
 
 int main() {
     // Crie arquivos de entrada e saída fictícios para testar as funções
-    FILE *input_file = fopen("input.txt", "rb");
-    FILE *output_file = fopen("output.txt", "wb");
+    FILE *input_file = fopen("utf2varint_entrada.txt", "wb");
+    FILE *output_file = fopen("utf2varint_entrada", "wb");
+
+    if (input_file == NULL) {
+            printf("Erro ao abrir o arquivo utf2varint_entrada.txt\n");
+            return 1;
+        }
+
+    if (output_file == NULL) {
+        printf("Erro ao abrir o arquivo utf2varint_entrada\n");
+        return 1;
+    }
+
+    fputc(0x00A9, input_file);
+
+    fclose(input_file);
+    input_file = fopen("utf2varint_entrada.txt", "rb");
 
     // Teste utf2varint
-    assert(utf2varint(input_file, output_file) == 0);
+    utf2varint(input_file, output_file);
 
     // Feche e reabra os arquivos para a função varint2utf
     fclose(input_file);
     fclose(output_file);
 
-    input_file = fopen("output.txt", "rb");
-    output_file = fopen("result.txt", "wb");
+    input_file = fopen("varint2utf_entrada", "rb");
+    output_file = fopen("varint2utf_saida.txt", "wb");
+
+
+    // Crie a sequência de bytes correspondente ao caractere © em UTF-8
+    unsigned char utf8_bytes[] = {0xC2, 0xA9};
+
+    // Escreva a sequência de bytes no arquivo
+    fwrite(utf8_bytes, sizeof(unsigned char), sizeof(utf8_bytes), input_file);
+
+    fclose(input_file);
+    input_file = fopen("varint2utf_entrada", "rb");
+    
 
     // Teste varint2utf
-    assert(varint2utf(input_file, output_file) == 0);
+    varint2utf(input_file, output_file);
 
     // Feche os arquivos
     fclose(input_file);
     fclose(output_file);
 
     // Abra os arquivos de entrada e resultado para verificar a consistência dos dados
-    input_file = fopen("input.txt", "rb");
-    FILE *result_file = fopen("result.txt", "rb");
+    input_file = fopen("utf2varint_entrada.txt", "rb");
+    output_file = fopen("varint2utf_saida.txt", "rb");
 
     // Compare o conteúdo dos arquivos de entrada e resultado
     int ch1, ch2;
-    while ((ch1 = fgetc(input_file)) != EOF && (ch2 = fgetc(result_file)) != EOF) {
+    while ((ch1 = fgetc(input_file)) != EOF && (ch2 = fgetc(output_file)) != EOF) {
         assert(ch1 == ch2);
     }
 
     // Feche os arquivos de entrada e resultado
     fclose(input_file);
-    fclose(result_file);
+    fclose(output_file);
 
     printf("Testes unitários concluídos com sucesso.\n");
 
